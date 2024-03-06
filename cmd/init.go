05fa8681b258994a4352818432f77b89ac7b5aaa
Psykka <36780964+Psykka@users.noreply.github.com>
2024-03-06 00:42:47 -0300

    feat: init command

--- cmd/init.go
+++ cmd/init.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/spf13/cobra"
 )
@@ -14,6 +15,27 @@ var initCmd = &cobra.Command{
 	Use:   "init",
 	Short: "Create config file for migrash",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Print("Init")
+		f, err := os.Create(".migrashrc")
+
+		if err != nil {
+			panic(err)
+		}
+
+		defer f.Close()
+
+		initialConfig := `# example config file
+
+MIGRATION_DIR=migrations
+# DATABASE_URL=$DATABASE # from env
+DATABASE_URL=mysql://root:password@localhost:3306/migrash
+MIGRATION_TABLE=_migrash_migrations`
+
+		_, err = f.WriteString(initialConfig)
+
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println("Initial config file created!")
 	},
 }
